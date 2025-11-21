796b7ff61f5987979ec6a1c9516f1e463b3b7032
Andrew Hemming <andrew@footprintit.net>
2025-11-21 14:50:03 +0000

    Revert "Update variables.tf"
    This reverts commit 34e94a5194a851f422f1272872125e9585628ad5.

--- variables.tf
+++ variables.tf
@@ -61,6 +61,6 @@ variable "volume_size" {
 
 variable "block_device_mappings" {
   description = "Block device mappings for the EC2NodeClasses"
-  type        = string
-  default     = ""
+  type        = any
+  default     = null
 }
